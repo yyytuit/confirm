31e9979aa03cda1bebb6732b75fb612d5bd3f2c5
吉松寛道 <yyytuit@yahoo.co.jp>
2020-09-07 09:57:09 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'registrations#index'
   resources :registrations, only: [:index, :new, :create] do
     collection do
+      get :confirm
       post :confirm
       get :finish
     end
